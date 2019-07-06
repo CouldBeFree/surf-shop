d6a6e45c60b9140bd62ed096161775c91d218377
CouldBeFree <propokerstar90@gmail.com>
2019-07-06 22:55:36 +0300

    enable authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
+const mongoose = require('mongoose');
 
 // require routes
 const index = require('./routes/index');
@@ -14,6 +15,15 @@ const reviews = require('./routes/reviews');
 const posts = require('./routes/posts');
 
 const app = express();
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/surf-shop', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connected')
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
