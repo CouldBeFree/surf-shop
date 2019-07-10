60f943c94155b07fa99c6c1aba0af7df299e4569
sasha <propokerstar90@gmail.com>
2019-07-10 17:53:51 +0300

    add location to post

--- mapbox-test.js
+++ mapbox-test.js
@@ -6,12 +6,12 @@ const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 async function geocoder(location){
     // match.features[0].geometry.coordinates
     try{
-        let respone = await geocodingClient.forwardGeocode({
+        let response = await geocodingClient.forwardGeocode({
             query: location,
             limit: 1
         })
             .send();
-        console.log(respone.body.features[0].geometry.coordinates);
+        console.log(response.body.features[0].geometry.coordinates);
     } catch (error) {
         console.log(error.message)
     }
