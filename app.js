314f85f47dbb522bcda4f8a32216b744745c44cd
sasha <propokerstar90@gmail.com>
2019-07-12 16:46:20 +0300

    commit

--- app.js
+++ app.js
@@ -56,6 +56,17 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// set local variables middleware
+app.use(function (req, res, next) {
+  // success flash message
+  res.locals.success = req.session.success || '';
+  delete req.session.success;
+  // error flash message
+  res.locals.error = req.session.error || '';
+  delete req.session.error;
+  next();
+});
+
 // Mount routes
 app.use('/', index);
 app.use('/posts', posts);
@@ -69,12 +80,14 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  /*res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error');*/
+  req.session.error = err.message;
+  res.redirect('back')
 });
 
 module.exports = app;
